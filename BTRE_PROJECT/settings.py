f941b5ab60672362358e67ffc479b67be319849f
kalmastenitin <nknckalmaste@gmail.com>
2019-01-31 18:45:36 +0530

    Contact Form added with Email Functionality

--- BTRE_PROJECT/settings.py
+++ BTRE_PROJECT/settings.py
@@ -135,3 +135,12 @@ STATICFILES_DIRS = [
 #Media folder settings
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
+
+#Email config
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = '587'
+EMAIL_HOST_USER = 'nknckalmaste@gmail.com'
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = True
+
+#messages
--- pages/views.py
+++ pages/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from listings.choices import price_choices,bedroom_choices,state_choices
 from listings.models import Listing
+from django.contrib import messages
+from django.core.mail import send_mail
 
 # Create your views here.
 def index(request):
@@ -20,3 +22,20 @@ def about(request):
     'listings': listings,
     }
     return render(request, 'pages/about.html', context)
+
+def contact(request):
+    if request.method == 'POST':        
+        name = request.POST.get('name')
+        contact_num = request.POST.get('number')
+        email_id = request.POST.get('email')
+        inquiry_asked = request.POST.get('message')
+        #send_mail
+        send_mail(
+            'Real Estate Site Enquiry',
+            inquiry_asked,
+            'nknckalmaste@gmail.com',
+            [email_id ],
+            fail_silently=False
+        )
+
+    return render(request, 'pages/contact.html')
